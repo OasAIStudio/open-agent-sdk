0e323dc2020e7c063d751c8938b6fcd900fd77c2
octane0411 <wdznb1@gmail.com>
2026-03-03 21:23:30 +0800

    chore(web): remove next build artifacts from git

--- packages/web/.next/static/chunks/pages/_app-502549203971a842.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2156:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(387)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2156),n(2587))),_N_E=_.O()}]);
